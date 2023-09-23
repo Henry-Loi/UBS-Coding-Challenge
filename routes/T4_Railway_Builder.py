25c13e79e6eb3a39e266f53e90487bf3d767a191
Henry-Loi <91612066+Henry-Loi@users.noreply.github.com>
2023-09-23 09:42:10 +0800

    push first draft of Railway

---
+++ routes/T4_Railway_Builder.py
@@ -0,0 +1,35 @@
+import json
+import logging
+
+from flask import request
+
+from routes import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/evalute', methods=['POST'])
+def evaluate():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    
+    # my code starts here
+    result = []
+    
+    for entry in data:
+        values = entry.split(", ")
+        length_of_railway = int(values[0])
+        number_of_types_of_track_piece = int(values[1]) # this is actually useless
+        track_pieces = list(map(int, values[2:]))
+        
+        combinations = [0] * (length_of_railway + 1)
+        combinations[0] = 1 # initial count (itself)
+        
+        for piece in track_pieces:
+            for i in range(piece, length_of_railway + 1):
+                combinations[i] += combinations[i - piece]
+        
+        result.append(combinations[-1])
+    # my code ends here
+
+    logging.info("My result :{}".format(result))
+    return json.dumps(result)
--- routes/__init__.py
+++ routes/__init__.py
@@ -2,4 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 # import routes.square
-import routes.lazy_developer
+# import routes.T0_lazy_developer
+import routes.T4_Railway_Builder
