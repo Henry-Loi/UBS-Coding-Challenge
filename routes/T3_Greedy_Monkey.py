38b6f4fc964454694279bb40c60933b88010fd77
Henry-Loi <91612066+Henry-Loi@users.noreply.github.com>
2023-09-23 15:13:14 +0800

    use multi-variable to make it faster

--- routes/T3_Greedy_Monkey.py
+++ routes/T3_Greedy_Monkey.py
@@ -15,11 +15,7 @@ def testing():
     # logging.info("data sent for evaluation {}".format(data))
     
     # my code starts here
-    result = 0
-
-    n = len(data["f"]) # the number of fruit
-    max_w = data["w"] # the maximum weight of fruit
-    max_v = data["v"] # the maximum volume of fruit
+    result, n, max_w, max_v = 0, len(data["f"]), data["w"], data["v"]
 
     # try try top down dp
     dp = [[0 for _ in range(max_v+1)]  for __ in range(max_w+1)] # dp[i][j] is the max val for weight i and volume j
@@ -29,9 +25,7 @@ def testing():
         for j in range(max_w, w-1, -1): # for each weight (min is w so won't give negative index)
             for k in range(max_v, v-1, -1): # for each volume (min is v so won't give negative index)
                 dp[j][k] = max(dp[j][k], dp[j-w][k-v] + val)
-
-    result = dp[max_w][max_v]
     # my code ends here
 
     # logging.info("My result :{}".format(result))
-    return json.dumps(result)
+    return json.dumps(dp[max_w][max_v])
