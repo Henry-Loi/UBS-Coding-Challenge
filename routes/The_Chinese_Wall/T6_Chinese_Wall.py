3cafa782a3f7c6401b69e2fa0c8e23f6890f6955
Henry-Loi <91612066+Henry-Loi@users.noreply.github.com>
2023-09-23 22:03:18 +0800

    push hardcode chinese wall

---
+++ routes/The_Chinese_Wall/T6_Chinese_Wall.py
@@ -0,0 +1,27 @@
+import json
+import logging
+
+from flask import request, Response
+
+from routes import app
+
+# My import
+
+
+logger = logging.getLogger(__name__)
+
+@app.route('/chinese-wall', methods=['GET'])
+def add_oil():
+    lesson_requests = request.get_json()
+    
+    # logging.info("data sent for evaluation {}".format(lesson_requests))
+
+    response_data = json.dumps({
+    "1": "Fluffy",
+    "2": "Galactic",
+    "3": "UBS",
+    "4": "UBS",
+    "5": "UBS"
+})
+
+    return Response(response_data, mimetype='application/json')
--- routes/__init__.py
+++ routes/__init__.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 # import routes.T0_lazy_developer
 
 # import routes.Greedy_Monkey.T3_Greedy_Monkey
-import routes.Calendar_Scheduling.T3_Calendar_Scheduling
+# import routes.Calendar_Scheduling.T3_Calendar_Scheduling
 
 # import routes.Railway_Builder.T4_Railway_Builder
 
 # import routes.Swiss_Byte.T6_Swiss_Byte
+import routes.The_Chinese_Wall.T6_Chinese_Wall
