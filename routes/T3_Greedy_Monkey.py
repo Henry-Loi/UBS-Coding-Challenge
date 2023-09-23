87812233a5dfce8100b2ddf7c8fe2404b76eaa36
Henry-Loi <91612066+Henry-Loi@users.noreply.github.com>
2023-09-23 15:59:14 +0800

    use functools

--- routes/T3_Greedy_Monkey.py
+++ routes/T3_Greedy_Monkey.py
@@ -6,7 +6,11 @@ from flask import request
 from routes import app
 
 # My import
-import numpy as np
+import functools
+
+@functools.lru_cache
+def dp(i, j):
+    return dp[i][j]
 
 logger = logging.getLogger(__name__)
 
@@ -16,10 +20,10 @@ def testing():
     # logging.info("data sent for evaluation {}".format(data))
     
     # my code starts here
-    result, n, max_w, max_v = 0, len(data["f"]), data["w"], data["v"]
+    n, max_w, max_v = len(data["f"]), data["w"], data["v"]
 
     # try try top down dp
-    dp = np.zeros((max_w+1, max_v+1)) # dp[i][j] is the max val for weight i and volume j
+    dp = [[0 for _ in range(max_v+1)]  for __ in range(max_w+1)] # dp[i][j] is the max val for weight i and volume j
 
     for i in range(1, n+1): # for each fruit
         w, v, val = data["f"][i-1] # weight, volume, value of the fruit
@@ -29,4 +33,4 @@ def testing():
     # my code ends here
 
     # logging.info("My result :{}".format(result))
-    return json.dumps(int(dp[max_w][max_v]))
+    return json.dumps(dp[max_w][max_v])
